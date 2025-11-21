7d03f798f60bebf58267500ba0d3cb19a9180f8a
ehaezebroeck18 <ehaezebroeck@uiowa.edu>
2025-11-21 08:59:53 -0600

    Completed string fix

--- WebsiteDes/website/server.js
+++ WebsiteDes/website/server.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "***YOUR ROOT MYSQL PASSWORD GOES HERE***"
+  password: "***YOUR ROOT MYSQL PASSWORD GOES HERE***",
   database: "test"
 });
 
